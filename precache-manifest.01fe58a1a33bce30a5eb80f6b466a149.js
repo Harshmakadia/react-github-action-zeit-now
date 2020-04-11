b31c0888aae1595f7b833c69438cf1e37f4f5124
Harsh Makadia <harsh.makadia@marutitech.com>
2020-04-11 13:25:09 +0530

    Updates

---
+++ precache-manifest.01fe58a1a33bce30a5eb80f6b466a149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fe0fa5e90c325b6b85988812653100",
+    "url": "/react-github-action-zeit-now/index.html"
+  },
+  {
+    "revision": "5c65a80234e16bec8695",
+    "url": "/react-github-action-zeit-now/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1fd55ac2493c11ecf61",
+    "url": "/react-github-action-zeit-now/static/js/2.62342ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-action-zeit-now/static/js/2.62342ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c65a80234e16bec8695",
+    "url": "/react-github-action-zeit-now/static/js/main.9de94540.chunk.js"
+  },
+  {
+    "revision": "fffebc464d957065a693",
+    "url": "/react-github-action-zeit-now/static/js/runtime-main.010d77f7.js"
+  }
+]);
