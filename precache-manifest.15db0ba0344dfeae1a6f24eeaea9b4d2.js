67bcf98c0f3519d996ebd8701af9b63ef065123c
Harshmakadia <harsh.makadia@marutitech.com>
2020-04-11 09:35:02 +0000

    Updates

---
+++ precache-manifest.15db0ba0344dfeae1a6f24eeaea9b4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c436e628460beb2f9e4a8c1f2f4361bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38e0b90122cd44f76da2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1fd55ac2493c11ecf61",
+    "url": "/static/js/2.62342ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62342ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e0b90122cd44f76da2",
+    "url": "/static/js/main.bd229984.chunk.js"
+  },
+  {
+    "revision": "1a98f6a2c5adc5b8d340",
+    "url": "/static/js/runtime-main.c86f77df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-github-action-zeit-now/precache-manifest.85a236df6e336f35e95738f669a1ef04.js"
+  "/precache-manifest.15db0ba0344dfeae1a6f24eeaea9b4d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-github-action-zeit-now/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
